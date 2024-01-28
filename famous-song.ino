43a8b583479ca07f35142527632bf176f5616048
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2024-01-28 15:14:35 +0200

    Update famous-song.ino

--- famous-song.ino
+++ famous-song.ino
@@ -1,26 +1,6 @@
 #include "DigiKeyboard.h"
-#include <Windows.h>
-
-bool isEnglishLayout(HKL layout) {
-    // The English layout code is "00000409"
-    return (layout == LoadKeyboardLayout(L"00000409", KLF_NOTELLSHELL));
-}
 
 void setup() {
-    // Get the current keyboard layout
-    HKL currentLayout = GetKeyboardLayout(0);
-
-    // Check if the current layout is English
-    if (!isEnglishLayout(currentLayout)) {
-        // Attempt to set the keyboard layout to English
-        HKL englishLayout = LoadKeyboardLayout(L"00000409", KLF_ACTIVATE | KLF_SUBSTITUTE_OK);
-        if (englishLayout != NULL) {
-            DigiKeyboard.print("Changed keyboard layout to English. ");
-        } else {
-            DigiKeyboard.print("Failed to change keyboard layout. ");
-        }
-    }
-
     // Play the famous song
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(600);
