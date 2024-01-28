5880986c0c874a15ec51ce8153df783d8fc9ed55
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2024-01-28 14:05:56 +0200

    Update famous-song.ino

--- famous-song.ino
+++ famous-song.ino
@@ -24,7 +24,7 @@ void setup() {
     // Play the famous song
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(600);
-    DigiKeyboard.print("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley&autoplay=1");
+    DigiKeyboard.print("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(200);
     DigiKeyboard.sendKeyStroke(KEY_F11);
